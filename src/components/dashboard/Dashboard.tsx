1dc7b95a87df7839cc54e125d328a4dfb0573ea5
Lopson <balzhinimaev-2011@mail.ru>
2020-05-05 17:05:00 +0900

    Added Notifications.tsx and ProjectList.tsx components to Dashboard.tsx

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Notifications from "./Notifications";
+import ProjectList from "../project/ProjectList";
 
-const Dashboard = () => {
-	return (
-		<div>
-		
-		</div>
-	)
+class Dashboard extends Component<any, any>{
+	render() {
+		return (
+			<div className={'dashboard container'}>
+				<div className="row">
+					<div className="col s12 m6">
+						<ProjectList/>
+					</div>
+					<div className="col s12 m5 offset-m1">
+						<Notifications/>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Dashboard;
