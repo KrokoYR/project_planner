f46ea20e484b80520c512127ec0503ad4ea7f893
Lopson <balzhinimaev-2011@mail.ru>
2020-05-05 17:02:57 +0900

    Add ProjectSummary component to ProjectList.tsx

--- src/components/project/ProjectList.tsx
+++ src/components/project/ProjectList.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
 	return (
-		<div>
-		
+		<div className={'project-list section'}>
+			<div className="card z-depth-0 project-summary">
+				
+				<ProjectSummary/>
+				<ProjectSummary/>
+				<ProjectSummary/>
+				<ProjectSummary/>
+				
+			</div>
 		</div>
 	)
 }
