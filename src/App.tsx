c7c0f106081a9b84fb16663c6d55fbf6ee27b9ca
Lopson <balzhinimaev-2011@mail.ru>
2020-05-05 17:06:27 +0900

    Added Dashboard, SignIn/SignUp, ProjectDetails component as routs to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+import ProjectDetails from "./components/project/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from './components/auth/SignUp';
 
 class App extends Component<any, any>{
 	render() {
@@ -8,6 +12,12 @@ class App extends Component<any, any>{
 			<BrowserRouter>
 				<div className="App">
 					<Navbar/>
+					<Switch>
+						<Route exact path={'/dashboard'} component={Dashboard}/>
+						<Route path={'/project/:id'} component={ProjectDetails}/>
+						<Route path={'/sign-in'} component={SignIn} />
+						<Route path={'/sign-up'} component={SignUp} />
+					</Switch>
 				</div>
 			</BrowserRouter>
 		);
