e3f314fd986499d03a05f69bda1a89cc5cd8a104
Lopson <balzhinimaev-2011@mail.ru>
2020-05-05 17:02:33 +0900

    Update ProjectSummary.tsx to display project's information by id

--- src/components/project/ProjectSummary.tsx
+++ src/components/project/ProjectSummary.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const ProjectSummary = () => {
 	return (
-		<div>
-		
+		<div className={'card-content grey-text text-darken-3'}>
+			<span className={'card-title'}>Project title</span>
+			<p>Posted by the Lopson</p>
+			<p className={'grey-text'}>3rd of September, 2am</p>
 		</div>
 	)
 }
