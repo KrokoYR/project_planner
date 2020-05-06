23c11892a180a4dbae6822518fbe9faab05e5a32
Lopson <balzhinimaev-2011@mail.ru>
2020-05-06 11:25:01 +0900

    Changed class components to functional components with hooks(SignIn.ts/SignUp.ts).

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,47 +1,46 @@
-import React, {ChangeEvent, FormEvent} from 'react';
+import React, {ChangeEvent, FC, FormEvent, useState} from 'react';
 
-class SignIn extends React.Component {
+const SignIn: FC<any> =() => {
 	
-	state = {
-		email: "",
-		password: ""
-	}
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 	
-	handleChange = (e:ChangeEvent<HTMLInputElement>) => {
-		this.setState({
-			[e.target.id]: e.target.value
-		})
-	}
+	// HTML ids for inputs:
+	const EMAIL_ID = "email";
+	const PASSWORD_ID = "password";
 	
-	handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log(this.state)
+		console.log(email, password);
 	}
 	
-	render() {
-		return (
-			<div className={'container'}>
-				<form onSubmit={this.handleSubmit} className="white">
-					<h5 className="grey-text text-darken-3">Sign In</h5>
-					
-					<div className="input-field">
-						<label htmlFor="email">Email</label>
-						<input type="email" id={"email"} onChange={this.handleChange}/>
-					</div>
-					
-					<div className="input-field">
-						<label htmlFor="password">Password</label>
-						<input type="password" id={"password"} onChange={this.handleChange}/>
-					</div>
-					
-					<div className="input-field">
-						<button className="btn ping lighten-1 z-depth-0">Login</button>
-					</div>
-					
-				</form>
-			</div>
-		)
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		if (e.target.id === EMAIL_ID) setEmail(e.target.value);
+		else if(e.target.id === PASSWORD_ID) setPassword(e.target.value);
 	}
+	
+	return (
+		<div className={'container'}>
+			<form onSubmit={handleSubmit} className="white">
+				<h5 className="grey-text text-darken-3">Sign In</h5>
+				
+				<div className="input-field">
+					<label htmlFor="email">Email</label>
+					<input type="email" id={EMAIL_ID} onChange={handleChange}/>
+				</div>
+				
+				<div className="input-field">
+					<label htmlFor="password">Password</label>
+					<input type="password" id={PASSWORD_ID} onChange={handleChange}/>
+				</div>
+				
+				<div className="input-field">
+					<button className="btn ping lighten-1 z-depth-0">Login</button>
+				</div>
+				
+			</form>
+		</div>
+	)
 }
 
 export default SignIn;
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,59 +1,63 @@
-import React, {ChangeEvent, FormEvent} from 'react';
+import React, {ChangeEvent, FC, FormEvent, useState} from 'react';
 
-class SignUp extends React.Component {
+const SignUp: FC<any> = () => {
 	
-	state = {
-		email: "",
-		password: "",
-		firstName: "",
-		lastName: ""
+	// Component state:
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [lastName, setLastName] = useState('')
+	const [firstName, setFirstName] = useState('')
+	
+	// HTML ids for inputs:
+	const EMAIL_ID = "email";
+	const PASSWORD_ID = "password";
+	const LAST_NAME_ID = "lastName";
+	const FIRST_NAME_ID = "firstName";
+	
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		if(e.target.id === EMAIL_ID) setEmail(e.target.value);
+		else if(e.target.id === PASSWORD_ID) setPassword(e.target.value);
+		else if(e.target.id === LAST_NAME_ID) setLastName(e.target.value);
+		else if(e.target.id === FIRST_NAME_ID) setFirstName(e.target.value);
 	}
 	
-	handleChange = (e:ChangeEvent<HTMLInputElement>) => {
-		this.setState({
-			[e.target.id]: e.target.value
-		})
-	}
-	
-	handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log(this.state)
+		console.log(email, password, lastName, firstName);
 	}
 	
-	render() {
-		return (
-			<div className={'container'}>
-				<form onSubmit={this.handleSubmit} className="white">
-					<h5 className="grey-text text-darken-3">Sign Up</h5>
-					
-					<div className="input-field">
-						<label htmlFor="email">Email</label>
-						<input type="email" id={"email"} onChange={this.handleChange}/>
-					</div>
-					
-					<div className="input-field">
-						<label htmlFor="password">Password</label>
-						<input type="password" id={"password"} onChange={this.handleChange}/>
-					</div>
-					
-					<div className="input-field">
-						<label htmlFor="lastName">Last Name</label>
-						<input type="text" id={"lastName"} onChange={this.handleChange}/>
-					</div>
-					
-					<div className="input-field">
-						<label htmlFor="firstName">First Name</label>
-						<input type="text" id={"firstName"} onChange={this.handleChange}/>
-					</div>
-					
-					<div className="input-field">
-						<button className="btn ping lighten-1 z-depth-0">Sign up</button>
-					</div>
+	return (
+		<div className={'container'}>
+			<form onSubmit={handleSubmit} className="white">
+				<h5 className="grey-text text-darken-3">Sign Up</h5>
 				
-				</form>
-			</div>
-		)
-	}
+				<div className="input-field">
+					<label htmlFor="email">Email</label>
+					<input type="email" id={EMAIL_ID} onChange={handleChange}/>
+				</div>
+				
+				<div className="input-field">
+					<label htmlFor="password">Password</label>
+					<input type="password" id={PASSWORD_ID} onChange={handleChange}/>
+				</div>
+				
+				<div className="input-field">
+					<label htmlFor="lastName">Last Name</label>
+					<input type="text" id={LAST_NAME_ID} onChange={handleChange}/>
+				</div>
+				
+				<div className="input-field">
+					<label htmlFor="firstName">First Name</label>
+					<input type="text" id={FIRST_NAME_ID} onChange={handleChange}/>
+				</div>
+				
+				<div className="input-field">
+					<button className="btn ping lighten-1 z-depth-0">Sign up</button>
+				</div>
+			
+			</form>
+		</div>
+	)
 }
 
 export default SignUp;
