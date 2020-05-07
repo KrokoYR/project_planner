9c4866598c2ef08a81e6283c9e1fb5e61270f0ab
Lopson <balzhinimaev-2011@mail.ru>
2020-05-07 15:14:18 +0900

    Added SignOut function to handle user's signOut action

--- src/components/layout/SignedInLinks.tsx
+++ src/components/layout/SignedInLinks.tsx
@@ -1,31 +1,35 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, {FC} from 'react';
+import {NavLink} from 'react-router-dom';
 import {AppActions} from "../../store";
 import {ThunkDispatch} from "redux-thunk";
 import {bindActionCreators} from "redux";
 import {thunkSingOut} from "../../store/reducers/auth/actions";
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
-const SignedInLinks = () => {
-    return (
-        <ul className="right">
-            <li>
-                <NavLink to={'/create-project'}>New project</NavLink>
-            </li>
-            <li>
-                <NavLink to={'/'}>Log Out</NavLink>
-            </li>
-            <li>
-                <NavLink to={'/'} className={'btn btn-floating pink lighten-1'}>NN</NavLink>
-            </li>
-        </ul>
-    )
+type Props = {
+	thunkSignOut: () => void
+}
+
+const SignedInLinks: FC<Props> = ({thunkSignOut}) => {
+	return (
+		<ul className="right">
+			<li>
+				<NavLink to={'/create-project'}>New project</NavLink>
+			</li>
+			<li>
+				<a href='#' onClick={thunkSignOut}>Log Out</a>
+			</li>
+			<li>
+				<NavLink to={'/'} className={'btn btn-floating pink lighten-1'}>NN</NavLink>
+			</li>
+		</ul>
+	)
 }
 
 const mapDispatchToProps = (
-    dispatch: ThunkDispatch<any, any, AppActions>
+	dispatch: ThunkDispatch<any, any, AppActions>
 ) => ({
-    thunkSignOut: bindActionCreators(thunkSingOut, dispatch)
+	thunkSignOut: bindActionCreators(thunkSingOut, dispatch)
 })
 
 export default connect(null, mapDispatchToProps)(SignedInLinks);
