51581e90d886e446560e7a60463e1b755f8aad21
Lopson <balzhinimaev-2011@mail.ru>
2020-05-07 13:48:49 +0900

    Added mapStateToProps to ProjectDetails.tsx to handle the right project

--- src/components/project/ProjectDetails.tsx
+++ src/components/project/ProjectDetails.tsx
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {AppState} from "../../store";
+import { connect } from 'react-redux';
+import {useFirestoreConnect} from "react-redux-firebase";
 
-const ProjectDetails = (props:any) => {
-	const id = props.match.params.id
-	return (
-		<div>
-			<div className="container section project-details">
-				<div className="card z-depth-0">
-					<div className="card-content">
+type Props = {
+	project: any
+}
+
+const ProjectDetails: FC<Props> = ({project}) => {
+	useFirestoreConnect('projects')
+	
+	if(project) {
+		return (
+			<div>
+				<div className="container section project-details">
+					<div className="card z-depth-0">
+						<div className="card-content">
 						<span className="card-title">
-							Project Title - {id}
+							{project.title}
 						</span>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem deleniti doloremque dolorum fugit illo inventore laudantium minus, nam necessitatibus quis repudiandae saepe sed sit soluta suscipit vel? Esse, recusandae? Animi assumenda corporis cumque ex exercitationem itaque necessitatibus, porro quasi quis, recusandae sed unde! Aliquam at consequuntur dolore dolorum enim laudantium molestiae sapiente. Corporis ducimus, earum eos labore rem vel.
-						</p>
-					</div>
-					<div className="card-action grey lighten-4 grey-text">
-						<div>Posted by Lopson</div>
-						<div>2nd September, 2am</div>
+							<p>
+								{project.content}
+							</p>
+						</div>
+						<div className="card-action grey lighten-4 grey-text">
+							<div>Posted by {project.authorFirstName} {project.authorLastName}</div>
+							<div>{new Date(project.createAt?.nanoseconds).toDateString()}</div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	} else {
+		return (
+			<div className={'container center'}>
+				<p>Loading project...</p>
+			</div>
+		)
+	}
+	
 }
 
-export default ProjectDetails;
+const mapStateToProps = (state: AppState, ownProps: any) => {
+	const id = ownProps.match.params.id
+	const projects = state.firestore.data.projects
+	const project = projects ? projects[id] : null
+	return {
+		project: project
+	}
+}
+
+export default connect(mapStateToProps)(ProjectDetails);
