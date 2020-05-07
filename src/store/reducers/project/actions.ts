4070ce797b9a04e9a1db22c9bfa55d09d8eee22d
Lopson <balzhinimaev-2011@mail.ru>
2020-05-07 13:42:13 +0900

    Added firestore to the thunk actions to get "Projects" from firebase

--- src/store/reducers/project/actions.ts
+++ src/store/reducers/project/actions.ts
@@ -1,23 +1,25 @@
-import {CREATE_PROJECT, PROJECT_ACTION_TYPES, PROJECT_STATE, PROJECT_TYPE} from "./types";
+import {CREATE_PROJECT, CREATE_PROJECT_ERROR, PROJECT_ACTION_TYPES, PROJECT_TYPE} from "./types";
 import {ActionCreator, Dispatch} from 'redux';
 import {AppActions, AppState} from "../../index";
+// uuid for creating unique id for projects:
 import { v4 as uuid } from 'uuid'
+//firebase:
+import {firebase} from '../../../index'
+import {getFirestore} from "redux-firestore";
 
-export const createProject: ActionCreator<PROJECT_ACTION_TYPES> = (project: PROJECT_TYPE)=> {
+export const createProject: ActionCreator<PROJECT_ACTION_TYPES> = (project: PROJECT_TYPE) => {
 	return {
 		type: CREATE_PROJECT,
 		payload: project
 	}
 }
 
-// const thunkCreateProject: ActionCreator<ThunkAction<void, AppState, null, Action<string>>> = (project: PROJECT_TYPE) => {
-// 	return (dispatch: Dispatch<AppState>): Action<PROJECT_ACTION_TYPES> => {
-// 		return dispatch({
-// 			type: CREATE_PROJECT,
-// 			payload: project
-// 		});
-// 	};
-// };
+export const createProjectError: ActionCreator<PROJECT_ACTION_TYPES> = (error: Error) => {
+	return {
+		type: CREATE_PROJECT_ERROR,
+		payload: error
+	}
+}
 
 export const thunkCreateProject = (projectData: PROJECT_TYPE) => {
 	return (dispatch: Dispatch<AppActions>, getState: () => AppState) => {
@@ -29,11 +31,26 @@ export const thunkCreateProject = (projectData: PROJECT_TYPE) => {
 		
 		const id = uuid();
 		
-		dispatch(
-			createProject({
-				id,
-				...project,
-			})
-		)
+		const firestore = getFirestore(firebase)
+		firestore.collection('projects').add({
+			...projectData,
+			authorId: id,
+			authorFirstName: "Lopson",
+			authorLastName: "Balzhinimaev",
+			createAt: new Date()
+		}).then(() => {
+			dispatch(
+				createProject({
+					id,
+					...project,
+				})
+			)
+		}).catch((error: Error)=>{
+			dispatch(
+				createProjectError(
+					error
+				)
+			)
+		})
 	}
 }
