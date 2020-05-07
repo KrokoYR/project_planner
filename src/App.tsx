a2a5564dde66c49d45e053968960185c310e4f1e
Lopson <balzhinimaev-2011@mail.ru>
2020-05-07 13:45:16 +0900

    Dashboard-container to Dashboard at App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ProjectDetails from "./components/project/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from './components/auth/SignUp';
 import { CreateProjectContainer } from "./components/project/CreateProject";
-import { DashboardContainer } from './components/dashboard/Dashboard';
+import Dashboard from './components/dashboard/Dashboard';
 
 
 class App extends Component<any, any>{
@@ -15,7 +15,7 @@ class App extends Component<any, any>{
 				<div className="App">
 					<Navbar/>
 					<Switch>
-						<Route exact path={'/'} component={DashboardContainer}/>
+						<Route exact path={'/'} component={Dashboard}/>
 						<Route path={'/project/:id'} component={ProjectDetails}/>
 						<Route path={'/sign-in'} component={SignIn} />
 						<Route path={'/sign-up'} component={SignUp} />
