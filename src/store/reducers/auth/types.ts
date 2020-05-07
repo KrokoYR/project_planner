4b4073a0dc6524589d1395bf83606722a2a6922c
Lopson <balzhinimaev-2011@mail.ru>
2020-05-07 13:42:49 +0900

    Added types.ts(TYPES) for auth-reducer.ts

--- src/store/reducers/auth/types.ts
+++ src/store/reducers/auth/types.ts
@@ -1,1 +1,40 @@
-export {}
+// Action types:
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_ERROR = 'LOGIN_ERROR'
+
+export const SIGN_OUT_SUCCESS = 'SIGN_OUT_SUCCESS'
+export const SIGN_OUT_ERROR = 'SIGN_OUT_ERROR'
+
+// Action interfaces:
+interface LOGIN_SUCCESS_ACTION {
+	type: typeof LOGIN_SUCCESS;
+}
+
+interface LOGIN_ERROR_ACTION {
+	type: typeof LOGIN_ERROR;
+	payload: Error;
+}
+
+interface SIGN_OUT_SUCCESS_ACTIONS {
+	type: typeof SIGN_OUT_SUCCESS;
+}
+
+interface SIGN_OUT_ERROR_ACTION {
+	type: typeof SIGN_OUT_ERROR;
+	payload: Error | null
+}
+
+export type AUTH_ACTION_TYPES = LOGIN_SUCCESS_ACTION |
+								LOGIN_ERROR_ACTION |
+								SIGN_OUT_SUCCESS_ACTIONS |
+								SIGN_OUT_ERROR_ACTION
+
+export interface CREDENTIAL_TYPE{
+	email: string;
+	password: string;
+}
+
+export interface AUTH_STATE {
+	loggedIn: boolean;
+	authError: Error | null;
+}
