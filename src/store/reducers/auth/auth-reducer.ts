ed77dc440a5977322b77951d205c46edb9168aff
Lopson <balzhinimaev-2011@mail.ru>
2020-05-07 13:45:36 +0900

    Created AuthReducer

--- src/store/reducers/auth/auth-reducer.ts
+++ src/store/reducers/auth/auth-reducer.ts
@@ -1,5 +1,47 @@
-const authReducer = (state:any, action:any) => {
-    return state;
+import {AUTH_ACTION_TYPES, AUTH_STATE, LOGIN_ERROR, LOGIN_SUCCESS, SIGN_OUT_ERROR, SIGN_OUT_SUCCESS} from "./types";
+
+const initState: AUTH_STATE = {
+    loggedIn: false,
+    authError: null
 }
 
-export default authReducer;
+const authReducer = (state = initState, action: AUTH_ACTION_TYPES) => {
+    switch (action.type) {
+        case LOGIN_ERROR: {
+            console.log('login error', action.payload)
+            return {
+                ...state,
+                loggedIn: false,
+                authError: action.payload
+            }
+        }
+        case LOGIN_SUCCESS: {
+            console.log("login success")
+            return {
+                ...state,
+                loggedIn: true,
+                authError: null
+            }
+        }
+        case SIGN_OUT_ERROR: {
+            console.log("sign out error")
+            return {
+                ...state,
+                loggedIn: true,
+                authError: action.payload
+            }
+        }
+        case SIGN_OUT_SUCCESS: {
+            console.log("Sign out succes")
+            return {
+                ...state,
+                loggedIn: false,
+            }
+        }
+        
+        default:
+            return state;
+    }
+}
+
+export {authReducer};
