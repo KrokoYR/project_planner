3653bffac8a6647da7e4cdb0be8c5d962b61d143
Lopson <balzhinimaev-2011@mail.ru>
2020-05-07 13:41:02 +0900

    Created actions for auth-reducer

--- src/store/reducers/auth/actions.ts
+++ src/store/reducers/auth/actions.ts
@@ -1,1 +1,51 @@
-export {}
+import {Dispatch} from "redux";
+import {
+	AUTH_ACTION_TYPES,
+	CREDENTIAL_TYPE,
+	LOGIN_ERROR,
+	LOGIN_SUCCESS,
+	SIGN_OUT_ERROR,
+	SIGN_OUT_SUCCESS
+} from "./types";
+import {AppState} from "../../index";
+import {getFirebase} from "react-redux-firebase";
+
+export const thunkSignIn = (credential: CREDENTIAL_TYPE) => {
+	//{ getFirebase, getFirestore }
+	return (dispatch: Dispatch<AUTH_ACTION_TYPES>, getState: () => AppState) => {
+		const firebase = getFirebase();
+		
+		firebase.auth().signInWithEmailAndPassword(
+			credential.email,
+			credential.password
+		).then(() => {
+			dispatch({
+				type: LOGIN_SUCCESS
+			})
+		}).catch((error: Error) => {
+			dispatch({
+				type: LOGIN_ERROR,
+				payload: error
+			})
+		})
+	}
+}
+
+export const SingOut = () => {
+	return (dispatch: Dispatch<AUTH_ACTION_TYPES>, getState: () => AppState) => {
+		const firebase = getFirebase()
+		
+		firebase.auth().signOut()
+			.then(() => {
+				dispatch({
+					type: SIGN_OUT_SUCCESS
+				})
+			})
+			.catch((error: Error) => {
+				dispatch({
+					type: SIGN_OUT_ERROR,
+					payload: error
+				})
+			})
+	}
+}
