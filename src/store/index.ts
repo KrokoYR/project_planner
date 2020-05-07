ca046b2db4841f3ac008d21473e13d738316019c
Lopson <balzhinimaev-2011@mail.ru>
2020-05-07 13:47:53 +0900

    Removed configureStore(creating store) function from index.ts(reducers folder). Now its at index.tsx file('/src')

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,11 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import authReducer from "./reducers/auth/auth-reducer";
-import {projectReducer} from "./reducers/project/project-reducer";
-import thunk, { ThunkMiddleware } from "redux-thunk";
-import { PROJECT_ACTION_TYPES } from "./reducers/project/types";
+//Types
+import {PROJECT_ACTION_TYPES} from "./reducers/project/types";
+import {AUTH_ACTION_TYPES} from "./reducers/auth/types";
 
-const rootReducer = combineReducers({
-	//auth: authReducer,
-	project: projectReducer
-})
+// Root reducer:
+import {rootReducer} from "./reducers/root-reducer";
 
+// App state and actions:
 export type AppState = ReturnType<typeof rootReducer>;
-export type AppActions = PROJECT_ACTION_TYPES
+export type AppActions = PROJECT_ACTION_TYPES | AUTH_ACTION_TYPES
 
-export const configureStore = () => {
-	const store = createStore(rootReducer, applyMiddleware(thunk as ThunkMiddleware<AppState, AppActions>))
-	return store;
-};
