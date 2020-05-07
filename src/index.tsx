55b6243845563520ee029f17b340dfe690902792
Lopson <balzhinimaev-2011@mail.ru>
2020-05-07 13:44:30 +0900

    Removed fbConfig.js/. Now firebase configuration at index.tsx(which is at '/src')

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// App and css:
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
-import {configureStore} from './store';
 
+// Service worker:
+import * as serviceWorker from './serviceWorker';
+
+// Redux provider:
+import { Provider } from 'react-redux'
+
+// Firebase provider:
+import {getFirebase, ReactReduxFirebaseProvider} from "react-redux-firebase";
+
+// Store configuration:
+import {createFirestoreInstance, getFirestore, reduxFirestore} from "redux-firestore";
+import {applyMiddleware, compose, createStore} from "redux";
+import {rootReducer} from "./store/reducers/root-reducer";
+import thunk from "redux-thunk";
+
+// Firebase imports:
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+// Firebase configuration:
+const firebaseConfig = {
+    apiKey: "AIzaSyD5eFbrhN5FVRhQSg2dp91qQFWc_tds9lI",
+    authDomain: "planner-bbe1b.firebaseapp.com",
+    databaseURL: "https://planner-bbe1b.firebaseio.com",
+    projectId: "planner-bbe1b",
+    storageBucket: "planner-bbe1b.appspot.com",
+    messagingSenderId: "930260250094",
+    appId: "1:930260250094:web:f2f470567428641256b2ff",
+    measurementId: "G-EX78EQR4GS"
+}
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+firebase.auth();
+export {firebase}
+
+// Store configuration:
+export const configureStore = () => {
+    const store = createStore(rootReducer,
+        compose(
+            applyMiddleware(thunk.withExtraArgument({
+                getFirebase,
+                getFirestore
+            })),
+            reduxFirestore(firebase),
+        )
+    )
+    
+    return store;
+};
 const store = configureStore()
+
+// react-redux-firebase config
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
+
+export const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance
+}
 
 ReactDOM.render(
   <Provider store={store} >
-    <App />
+      <ReactReduxFirebaseProvider {...rrfProps}>
+          <App />
+      </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
 );
