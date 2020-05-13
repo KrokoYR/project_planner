1ef739ceeefb79fc47d3e88db4d00f2a9b42827a
Lopson <balzhinimaev-2011@mail.ru>
2020-05-13 13:24:31 +0900

    Added authentication check before render the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import React, {Component} from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, {Component, FC, ReactElement, ReactNode} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from "./components/layout/Navbar";
 import ProjectDetails from "./components/project/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from './components/auth/SignUp';
-import { CreateProjectContainer } from "./components/project/CreateProject";
+import {CreateProjectContainer} from "./components/project/CreateProject";
 import Dashboard from './components/dashboard/Dashboard';
+import {FirebaseReducer, getFirebase, isLoaded} from "react-redux-firebase";
+import {connect} from "react-redux";
+import {AppState} from "./store";
 
+interface AuthIsReady {
+	auth: FirebaseReducer.AuthState
+}
 
-class App extends Component<any, any>{
+const dumbComponent: FC<AuthIsReady> = ({auth}) => {
+	
+	let elem = (<div>Loading screen</div>)
+	if(auth.isLoaded) elem = <App/>
+	return elem
+}
+
+const mapStateToProps = (state: AppState) => {
+	return {
+		auth: state.firebase.auth
+	}
+}
+
+export const AuthIsLoaded = connect(mapStateToProps)(dumbComponent);
+
+class App extends Component<any, any> {
+	
 	render() {
 		return (
 			<BrowserRouter>
@@ -17,8 +39,8 @@ class App extends Component<any, any>{
 					<Switch>
 						<Route exact path={'/'} component={Dashboard}/>
 						<Route path={'/project/:id'} component={ProjectDetails}/>
-						<Route path={'/sign-in'} component={SignIn} />
-						<Route path={'/sign-up'} component={SignUp} />
+						<Route path={'/sign-in'} component={SignIn}/>
+						<Route path={'/sign-up'} component={SignUp}/>
 						<Route path={'/create-project'} component={CreateProjectContainer}/>
 					</Switch>
 				</div>
