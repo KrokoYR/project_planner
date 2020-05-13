82f1d51be4cba7103171dd509a56762349360473
Lopson <balzhinimaev-2011@mail.ru>
2020-05-13 13:25:48 +0900

    Refactoring ProjectSummary.tsx

--- src/components/project/ProjectSummary.tsx
+++ src/components/project/ProjectSummary.tsx
@@ -1,7 +1,12 @@
 import React, {FC} from 'react';
-import { PROJECT_TYPE } from '../../store/reducers/project/types';
+import {PROJECT_TYPE} from '../../store/reducers/project/types';
 
-const ProjectSummary: FC<PROJECT_TYPE> = ({id, title, content, timestamp}) => {
+const ProjectSummary: FC<PROJECT_TYPE> = ({
+	                                          id,
+	                                          title,
+	                                          content,
+	                                          timestamp
+                                          }) => {
 	return (
 		<div className={'card-content grey-text text-darken-3'}>
 			<span className={'card-title'}>{title}</span>
