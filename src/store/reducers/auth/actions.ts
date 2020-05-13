f482750b96d3e6e2107a8f5eee8ea6130e16c041
Lopson <balzhinimaev-2011@mail.ru>
2020-05-13 15:57:48 +0900

    Added SignUp action

--- src/store/reducers/auth/actions.ts
+++ src/store/reducers/auth/actions.ts
@@ -4,10 +4,17 @@ import {
 	CREDENTIAL_TYPE,
 	LOGIN_ERROR,
 	LOGIN_SUCCESS,
-	SIGN_OUT_SUCCESS
+	NEW_USER_TYPE,
+	SIGN_OUT_SUCCESS,
+	SIGN_UP_ERROR,
+	SIGN_UP_SUCCESS,
 } from "./types";
+// Import state:
 import {AppState} from "../../index";
+// Get firebase:
 import {getFirebase} from "react-redux-firebase";
+import {getFirestore} from "redux-firestore";
+import {firebase} from '../../../index'
 
 export const thunkSignIn = (credential: CREDENTIAL_TYPE) => {
 	//{ getFirebase, getFirestore }
@@ -30,7 +37,8 @@ export const thunkSignIn = (credential: CREDENTIAL_TYPE) => {
 	}
 }
 
-export const thunkSingOut = () => {
+// Thunk to SignOut:
+export const thunkSignOut = () => {
 	return (dispatch: Dispatch<AUTH_ACTION_TYPES>, getState: () => AppState) => {
 		const firebase = getFirebase()
 		
@@ -40,5 +48,33 @@ export const thunkSingOut = () => {
 					type: SIGN_OUT_SUCCESS
 				})
 			})
+	}
+}
+
+// Thunk to SignUp:
+export const thunkSignUp = (newUser: NEW_USER_TYPE) => {
+	return (dispatch: Dispatch<AUTH_ACTION_TYPES>, getState: () => AppState) => {
+		const firestore = getFirestore(firebase);
+		const Firebase = getFirebase();
+		
+		Firebase.auth().createUserWithEmailAndPassword(
+			newUser.email,
+			newUser.password
+		).then((response) => {
+			return firestore.collection('users').doc(response.user?.uid).set({
+				firstName: newUser.firstName,
+				lastName: newUser.lastName,
+				initials: newUser.firstName[0] + newUser.lastName[0],
+			})
+		}).then(() => {
+			dispatch({
+				type: SIGN_UP_SUCCESS,
+			})
+		}).catch(err => {
+			dispatch({
+				type: SIGN_UP_ERROR,
+				payload: err,
+			})
+		})
 	}
 }
