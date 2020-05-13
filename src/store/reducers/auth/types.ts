3a572e07b80cb483b4cb6cc1ecfef7b02f7856e9
Lopson <balzhinimaev-2011@mail.ru>
2020-05-13 15:59:43 +0900

    Types for SignUp action

--- src/store/reducers/auth/types.ts
+++ src/store/reducers/auth/types.ts
@@ -4,6 +4,9 @@ export const LOGIN_ERROR = 'LOGIN_ERROR'
 
 export const SIGN_OUT_SUCCESS = 'SIGN_OUT_SUCCESS'
 
+export const SIGN_UP_SUCCESS = 'SIGN_UP'
+export const SIGN_UP_ERROR = 'SIGN_UP_ERROR'
+
 // Action interfaces:
 interface LOGIN_SUCCESS_ACTION {
 	type: typeof LOGIN_SUCCESS;
@@ -18,15 +21,36 @@ interface SIGN_OUT_SUCCESS_ACTIONS {
 	type: typeof SIGN_OUT_SUCCESS;
 }
 
-export type AUTH_ACTION_TYPES = LOGIN_SUCCESS_ACTION |
-								LOGIN_ERROR_ACTION |
-								SIGN_OUT_SUCCESS_ACTIONS
+interface SIGN_UP_SUCCESS_ACTION {
+	type: typeof SIGN_UP_SUCCESS;
+}
 
-export interface CREDENTIAL_TYPE{
+interface SIGN_UP_ERROR_ACTION {
+	type: typeof SIGN_UP_ERROR;
+	payload: Error;
+}
+
+export type AUTH_ACTION_TYPES = LOGIN_SUCCESS_ACTION |
+	LOGIN_ERROR_ACTION |
+	SIGN_OUT_SUCCESS_ACTIONS |
+	SIGN_UP_SUCCESS_ACTION |
+	SIGN_UP_ERROR_ACTION
+
+// Interface of inputs to LOGIN:
+export interface CREDENTIAL_TYPE {
 	email: string;
 	password: string;
 }
 
+// Interface of inputs to SIGN_UP:
+export interface NEW_USER_TYPE {
+	firstName: string;
+	lastName: string;
+	email: string;
+	password: string;
+}
+
+// Auth state:
 export interface AUTH_STATE {
 	loggedIn: boolean;
 	authError: Error | null;
